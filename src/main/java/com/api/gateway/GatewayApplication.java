76b611667099da080015ea75f02baaef7709ecf7
Derick Daniel <derickdaniel44@gmail.com>
2024-05-31 23:27:34 +0530

    API Gateway - Auth layer

--- src/main/java/com/api/gateway/GatewayApplication.java
+++ src/main/java/com/api/gateway/GatewayApplication.java
@@ -2,9 +2,6 @@ package com.api.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.gateway.route.RouteLocator;
-import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class GatewayApplication {
@@ -12,12 +9,4 @@ public class GatewayApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(GatewayApplication.class, args);
 	}
-
-	/*
-	 * @Bean public RouteLocator routerBuilder(RouteLocatorBuilder
-	 * routeLocatorBuilder) { return routeLocatorBuilder.routes()
-	 * .route("Microservice1", r ->
-	 * r.path("/serviceA/**").uri("http://localhost:8081/")) .route("Microservice2",
-	 * r -> r.path("/serviceB/**").uri("http://localhost:8082/")).build(); }
-	 */
 }
---
+++ src/main/java/com/api/gateway/filter/JwtAuthenticationFilter.java
@@ -0,0 +1,86 @@
+package com.api.gateway.filter;
+
+import java.util.List;
+import java.util.function.Predicate;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.gateway.filter.GatewayFilter;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.http.server.reactive.ServerHttpResponse;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+
+import com.api.gateway.exception.JwtTokenMalformedException;
+import com.api.gateway.exception.JwtTokenMissingException;
+import com.api.gateway.utils.JwtUtils;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.UnsupportedJwtException;
+import lombok.extern.slf4j.Slf4j;
+import reactor.core.publisher.Mono;
+
+@Component
+@Slf4j
+public class JwtAuthenticationFilter implements GatewayFilter {
+
+	Logger log = LoggerFactory.getLogger(JwtAuthenticationFilter.class);
+
+	@Autowired
+	private JwtUtils jwtUtils;
+
+	@Override
+	public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+
+		log.info("JwtAuthenticationFilter | filter is working");
+
+		ServerHttpRequest request = (ServerHttpRequest) exchange.getRequest();
+
+		final List<String> apiEndpoints = List.of("/signup", "/login", "/refreshtoken");
+
+		Predicate<ServerHttpRequest> isApiSecured = r -> apiEndpoints.stream()
+				.noneMatch(uri -> r.getURI().getPath().contains(uri));
+
+		log.info("JwtAuthenticationFilter | filter | isApiSecured.test(request) : " + isApiSecured.test(request));
+
+		if (isApiSecured.test(request)) {
+			if (!request.getHeaders().containsKey("Authorization")) {
+				ServerHttpResponse response = exchange.getResponse();
+				response.setStatusCode(HttpStatus.UNAUTHORIZED);
+
+				return response.setComplete();
+			}
+
+			final String authorization = request.getHeaders().getOrEmpty("Authorization").get(0);
+			final String token = authorization.replace("Bearer ", "");
+
+			log.info("JwtAuthenticationFilter | filter | token : " + token);
+
+			try {
+				jwtUtils.validateJwtToken(token);
+			} catch (ExpiredJwtException e) {
+				log.info("JwtAuthenticationFilter | filter | ExpiredJwtException | error : " + e.getMessage());
+				ServerHttpResponse response = exchange.getResponse();
+				response.setStatusCode(HttpStatus.UNAUTHORIZED);
+
+				return response.setComplete();
+
+			} catch (IllegalArgumentException | JwtTokenMalformedException | JwtTokenMissingException
+					| io.jsonwebtoken.SignatureException | UnsupportedJwtException e) {
+				ServerHttpResponse response = exchange.getResponse();
+				response.setStatusCode(HttpStatus.BAD_REQUEST);
+
+				return response.setComplete();
+			}
+
+			Claims claims = jwtUtils.getClaims(token);
+			exchange.getRequest().mutate().header("username", String.valueOf(claims.get("username"))).build();
+		}
+
+		return chain.filter(exchange);
+	}
+}
