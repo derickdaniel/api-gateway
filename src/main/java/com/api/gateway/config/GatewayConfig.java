901a9e4ec0130cc8bd020b330d7c9b1691683634
Derick Daniel <derickdaniel44@gmail.com>
2024-06-14 02:00:15 +0530

    Fixed Cors Issue

--- src/main/java/com/api/gateway/config/GatewayConfig.java
+++ src/main/java/com/api/gateway/config/GatewayConfig.java
@@ -5,6 +5,7 @@ import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import com.api.gateway.filter.JwtAuthenticationFilter;
 
@@ -18,7 +19,6 @@ public class GatewayConfig {
 	private JwtAuthenticationFilter filter;
 
 	public GatewayConfig(JwtAuthenticationFilter filter) {
-		super();
 		this.filter = filter;
 	}
 
--- src/main/java/com/api/gateway/config/SecurityConfig.java
+++ src/main/java/com/api/gateway/config/SecurityConfig.java
@@ -1,10 +1,16 @@
 package com.api.gateway.config;
 
+import java.util.Arrays;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsConfigurationSource;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
 
 @Configuration
 @EnableWebFluxSecurity
@@ -17,5 +23,16 @@ public class SecurityConfig {
 				.authorizeExchange(exchange -> exchange.anyExchange().permitAll());
 		return serverHttpSecurity.build();
 	}
-
+	
+	@Bean
+	public CorsConfigurationSource corsConfigurationSource() {
+		CorsConfiguration configuration = new CorsConfiguration();
+		configuration.setAllowedOrigins(Arrays.asList("*"));
+		configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
+		configuration.setAllowedHeaders(Arrays.asList("authorization", "content-type", "x-auth-token"));
+		configuration.setExposedHeaders(Arrays.asList("x-auth-token"));
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", configuration);
+		return source;
+	}
 }
